1334daacf9a986abc30b99b25e21ac112fcb3a24
nlangevin0-dev <nlangevin0@gmail.com>
2026-05-08 12:01:51 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,83 @@
+terraform {
+    required_version = ">= 1.5"
+
+    required_providers {
+      aws =  {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "kafka" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "${var.env}-vpc"
+    }
+}
+
+resource "aws_subnet" "kafka_sub1" {
+    vpc_id = aws_vpc.kafka.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "${var.aws_region}a"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "${var.env}-subnet-1"
+    }
+}
+
+resource "aws_internet_gateway" "kafka_igw" {
+    vpc_id = aws_vpc.kafka.id
+    tags = {
+        Name = "${var.env}-igw"
+    }
+}
+
+resource "aws_route_table" "kafka_rt" {
+    vpc_id = aws_vpc.kafka.id
+   
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.kafka_igw.id
+    }
+     tags = {
+        Name = "${var.env}-route-table"
+    }
+}
+
+resource "aws_security_group" "kafka_sg" {
+    name = "${var.env}-kafka-sg"
+    description = "Security group for Kafka cluster"
+    vpc_id = aws_vpc.kafka.id
+
+    ingress {
+        from_port = 9092
+        to_port = 9092
+        protocol = "tcp"
+        cidr_blocks = ["${var.home_ip}"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.home_ip}"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+resource "aws_route_table_association" "kafka_rta" {
+    subnet_id = aws_subnet.kafka_sub1.id
+    route_table_id = aws_route_table.kafka_rt.id
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "env" {
+  description = "The environment to deploy resources in (e.g., dev, staging, prod)."
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition    = contains(["dev", "staging", "prod"], var.env)
+    error_message = "The environment must be one of 'dev', 'staging', or 'prod'."
+  }
+}
+
+variable "home_ip" {
+    description = "Your home IP address for SSH access to the Kafka cluster."
+    type        = string
+}
+
+variable "instance_type" {
+    description = "The EC2 instance type for the Kafka cluster."
+    type        = string
+    default     = "t3.medium"
+}
